325f6baf5bebbfc9695ee62a21e368e5f92e338c
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-02-05 14:40:25 +0300

    Implement Graph

---
+++ structures/graph.js
@@ -0,0 +1,46 @@
+export default class Graph {
+  constructor() {
+    this.nodes = [];
+  }
+
+  /**
+   * Добавить узел к граф
+   * Сложность O(1)
+   *
+   * @param {any} value
+   */
+  addNode(value) {
+    this.nodes.push({
+      value,
+      lines: [],
+    });
+  }
+
+  /**
+   * Добавить узел к граф
+   * Сложность O(n)
+   *
+   * @param {any} value
+   * @returns {{value: any, lines: any[]}} node
+   */
+  find(value) {
+    return this.nodes.find(node => node.value === value);
+  }
+
+  /**
+   * Связать два узла между собой
+   * Сложность O(1)
+   *
+   * @param {any} startValue
+   * @param {any} endValue
+   */
+  addLine(startValue, endValue) {
+    const startNode = this.find(startValue);
+    const endNode = this.find(endValue);
+
+    if (!startNode || !endNode) {
+      throw new Error('Обе вершины должны существовать!');
+    }
+    startNode.lines.push(endNode);
+  }
+}
